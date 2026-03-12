23ee18f9c9bd9b9332b4ce108a4c5984f81d4f23
Sanskriti <asanskriti93@gmail.com>
2026-03-12 11:33:27 +0530

    Library Management Backend API

---
+++ controllers/bookController.js
@@ -0,0 +1,74 @@
+const Book = require("../models/Book");
+
+exports.addBook = async (req, res) => {
+  try {
+    const book = new Book(req.body);
+    const savedBook = await book.save();
+
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteBook = async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Book deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.searchBook = async (req, res) => {
+  try {
+    const title = req.query.title;
+
+    const books = await Book.find({
+      title: { $regex: title, $options: "i" }
+    });
+
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const bookRoutes = require("./routes/bookRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api", bookRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
