ec80bcd727071ed6c1ddc819eb068e025a5fcebc
Sanskriti571 <asanskriti93@gmail.com>
2026-03-17 15:35:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Database connection failed");
-    process.exit(1);
+    // process.exit(1);
   }
 };
 
